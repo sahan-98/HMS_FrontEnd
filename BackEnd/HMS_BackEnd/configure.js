8ac4b022c6ad5326e9bb04474d3b719c0bf25f8c
LasithEranga <lasitheranga1@gmail.com>
2023-08-24 06:51:51 +0530

    fix: db url

--- BackEnd/HMS_BackEnd/configure.js
+++ BackEnd/HMS_BackEnd/configure.js
@@ -4,8 +4,8 @@ require("dotenv").config({ path: __dirname + "/.env" });
 const MONGO_DB_PASSWORD = process.env["MONGO_DB_PASSWORD"];
 
 module.exports = {
-  // DB: `mongodb+srv://hospital_MS:${MONGO_DB_PASSWORD}@hospitalms.zw6pwlv.mongodb.net/?retryWrites=true&w=majority`,
-  DB: `mongodb://localhost:27017/hospitalms?retryWrites=true`,
+  DB: `mongodb+srv://hospital_MS:${MONGO_DB_PASSWORD}@hospitalms.zw6pwlv.mongodb.net/?retryWrites=true&w=majority`,
+  // DB: `mongodb://localhost:27017/hospitalms?retryWrites=true`,
 
   JWT_SECRET: "F9qy&s)?4=33s%$2h#F~ggg",
 };
